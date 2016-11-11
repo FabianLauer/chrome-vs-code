b47b2b3137adf67fefa6ca540896ada9e8352a7b
Fabian Lauer <kontakt@softwareschmiede-saar.de>
2016-11-11 23:45:41 +0100

    use node module 'mimetype' instead of 'mime'
    The 'mime' module couldn't be resolved when the extension was installed as a VSIX.

--- src/server/util/mime.ts
+++ src/server/util/mime.ts
@@ -1,12 +1,12 @@
-const mime = require('mime');
+const mimetype = require('mimetype');
 
 // override the default mime type:
-mime.default_type = 'tetx/plain';
+mimetype.default_type = 'tetx/plain';
 
 /**
  * Get the mime type associated with a file, if no mime type is found application/octet-stream is returned.
  * Performs a case-insensitive lookup using the extension in path (the substring after the last '/' or '.').
  */
 export function lookup(path: string): string {
-	return mime.lookup(path);
+	return mimetype.lookup(path);
 }
